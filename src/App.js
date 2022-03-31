89f077dcf8e6ebbc3edafdca0e7675c29b73463e
Binaryleo <leomouracss@fastmail.com>
2022-03-31 07:02:45 -0300

    initial files and router configurations

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route index element={<Home />} /> 
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element ={<List/>}/>
+              <Route path=":Userid" element={<Single/>}/>
+              <Route path="new" element={<New/>}/> 
+            </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function list(){
+    return (
+        <div>
+        <h1>My page list</h1>
+        </div>
+    )
+}
+
+export default list;
