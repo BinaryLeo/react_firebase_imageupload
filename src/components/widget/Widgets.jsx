7b5356bf91b904798c9060244d8548f318f0dbe5
Binaryleo <leomouracss@fastmail.com>
2022-04-03 18:34:38 -0300

    icons fix

--- src/components/widget/Widgets.jsx
+++ src/components/widget/Widgets.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowDown";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MonetizationOnOutlined  from '@mui/icons-material/MonetizationOnOutlined';
 const Widgets = ({type}) => {
@@ -26,7 +26,7 @@ const Widgets = ({type}) => {
                     title: "ORDERS",
                     isMoney:false,
                     link: "view al orders",
-                    icon: <ShoppingCartOutlinedIcon className="icon"  style={{color:"crimson", backgroundColor:"rgba(218,165,32,0.2)"}}/>,
+                    icon: <ShoppingCartOutlinedIcon className="icon"  style={{color:"goldenrod", backgroundColor:"rgba(218,165,32,0.2)"}}/>,
         };
                 break;
                 case "earning":
@@ -34,7 +34,7 @@ const Widgets = ({type}) => {
                         title: "EARNING",
                         isMoney:true,
                         link: "view net earnings",
-                        icon: <MonetizationOnOutlined className="icon"  style={{color:"crimson", backgroundColor:"rgba(255,0,0,0.2)"}}/>,
+                        icon: <MonetizationOnOutlined className="icon"  style={{color:"green", backgroundColor:"rgba(0,128,0,0.2)"}}/>,
             };
                     break;
                     case "balance":
@@ -42,7 +42,7 @@ const Widgets = ({type}) => {
                         title: "BALANCE",
                         isMoney:true,
                         link: "See details",
-                        icon: <AccountCircleOutlinedIcon className="icon"  style={{color:"crimson", backgroundColor:"rgba(255,0,0,0.2)"}}/>,
+                        icon: <AccountBalanceWalletOutlinedIcon className="icon"  style={{color:"purple", backgroundColor:"rgba(128,0,128,0.2)"}}/>,
             };
                     break;
             default:
