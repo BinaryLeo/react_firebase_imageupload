cc295d204627de6ab7f70ba1bc7b823bdc5eda21
Binaryleo <leomouracss@fastmail.com>
2022-04-03 01:05:22 -0300

    Navbar icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,23 +2,63 @@ import React from 'react'
 import  "./navbar.scss"
 import SearchIcon from '@mui/icons-material/Search';
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
  const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div classsName='wrapper'>
-       <div className="wrapper">
-       <input type="text" className="search" placeholder="Search..."/>
-       <SearchIcon className='icon' />
-       </div>
-        <div className="items">
-          <div className="item">
+    <div className="navbar">
+    <div className="wrapper">
+
+      <div className="search">
+        <input type="text" placeholder="Search..." />
+        <SearchIcon />
+      </div>
+
+      <div className="items">
+
+        <div className="item">
           <LanguageOutlinedIcon className="icon" />
-            English
-          </div>
+          English
         </div>
+
+        <div className="item">
+          <DarkModeOutlinedIcon
+            className="icon"
+          />
+        </div>
+
+        <div className="item">
+          <FullscreenExitOutlinedIcon className="icon" />
+        </div>
+
+        <div className="item">
+          <NotificationsNoneOutlinedIcon className="icon" />
+          <div className="counter">1</div>
+        </div>
+
+        <div className="item">
+          <ChatBubbleOutlineOutlinedIcon className="icon" />
+          <div className="counter">2</div>
+        </div>
+
+        <div className="item">
+          <ListOutlinedIcon className="icon" />
+        </div>
+       <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
+          </div>
       </div>
     </div>
-  )
-}
 
+
+  </div>
+);
+};
 export default Navbar;
