8dc80be1411d678b1b7d79cdfaf47142d3e9b29e
Binaryleo <leomouracss@fastmail.com>
2022-04-03 18:26:49 -0300

    widgets and icons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import './home.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Widgets  from '../../components/widget/Widgets'
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        Home Continer
+        <div className="widgets">
+          <Widgets type="user"/>
+          <Widgets type="order"/>
+          <Widgets type="earning"/>
+          <Widgets type="balance"/>
+        </div>
       </div>
     </div>
   )
