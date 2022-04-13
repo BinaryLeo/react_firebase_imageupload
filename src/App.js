1af5578084d0daeb6e3aafead9c3d65874a79370
Binaryleo <leomouracss@fastmail.com>
2022-04-12 23:41:26 -0300

    Home page comments

--- src/App.js
+++ src/App.js
@@ -3,27 +3,28 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/* yarn add react-router-dom */
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" />
-        <Route index element={<Home />} /> 
-            <Route path="login" element={<Login />} />
+        <Route index element={<Home />} />{/* Home is default */}
+            <Route path="login" element={<Login />} />{/* Login page */}
             <Route path="users">
               <Route index element ={<List/>}/>
-              <Route path=":Userid" element={<Single/>}/>
-              <Route path="new" element={<New/>}/> 
+              <Route path=":Userid" element={<Single/>}/>{/* Single user page */}
+              <Route path="new" element={<New/>}/> {/* New user page */}
             </Route>
             <Route path="products">
-              <Route index element ={<List/>}/>
-              <Route path=":Productid" element={<Single/>}/>
-              <Route path="new" element={<New/>}/>
+              <Route index element ={<List/>}/>{/* List of products */}
+              <Route path=":Productid" element={<Single/>}/>{/* Single product page */}
+              <Route path="new" element={<New/>}/>{/* New product page */}
             </Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
