e187e51358221c5e9451cabd8c8e46d20d888601
Binaryleo <leomouracss@fastmail.com>
2022-04-17 18:42:33 -0300

    add: firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "dashboard-fde25.firebaseapp.com",
-    projectId: "dashboard-fde25",
-    storageBucket: "dashboard-fde25.appspot.com",
-    messagingSenderId: "415034092206",
-    appId: "1:415034092206:web:f507bdd6109e396aebdb3f",
-    measurementId: "G-ZVXRE02YSH"
+    authDomain: "dashboard-88b0b.firebaseapp.com",
+
+    projectId: "dashboard-88b0b",
+
+    storageBucket: "dashboard-88b0b.appspot.com",
+
+    messagingSenderId: "6349174575",
+
+    appId: "1:6349174575:web:2228bea2b68826b6ea53da",
+
+    measurementId: "G-S2Y70LBTYP"
+
 };
 
 // Initialize Firebase
