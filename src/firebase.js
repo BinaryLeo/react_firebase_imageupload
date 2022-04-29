d376b31f2438bc49e1c3291751b2a1525339e279
Binaryleo <leomouracss@fastmail.com>
2022-04-28 21:17:04 -0300

    feat: firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
-import {getAuth} from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 
-const firebaseConfig = { // Firebase configuration
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: "dashboard-e8182.firebaseapp.com",
-    projectId: "dashboard-e8182",
-    storageBucket: "dashboard-e8182.appspot.com",
-    messagingSenderId: "615118146683",
-    appId: "1:615118146683:web:e9d5535a202a14d2bee6de",
-    measurementId: "G-EMLJ7NR12P"
-
-};
+const firebaseConfig = {
+  // Firebase configuration
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: 'dashboard-7b166.firebaseapp.com',
+  projectId: 'dashboard-7b166',
+  storageBucket: 'dashboard-7b166.appspot.com',
+  messagingSenderId: '314486544890',
+  appId: '1:314486544890:web:c21c8e12d818b9365d6c43',
+  measurementId: 'G-G2PS4LYB8S',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const db = getFirestore(app);
-export const auth = getAuth();
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
+export const analytics = getAnalytics(app)
+export const db = getFirestore(app)
+export const auth = getAuth()
+export const storage = getStorage(app)
