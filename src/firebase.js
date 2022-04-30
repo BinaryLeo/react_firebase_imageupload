1676846e4e663ef05130fca73b2092ac6d480f2f
Binaryleo <leomouracss@fastmail.com>
2022-04-29 23:02:48 -0300

    style: firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
-
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   // Firebase configuration
@@ -16,10 +15,9 @@ const firebaseConfig = {
   appId: '1:314486544890:web:c21c8e12d818b9365d6c43',
   measurementId: 'G-G2PS4LYB8S',
 }
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-export const analytics = getAnalytics(app)
-export const db = getFirestore(app)
-export const auth = getAuth()
-export const storage = getStorage(app)
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
